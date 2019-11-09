0c990fab3ab6a20fb077fe5f99d400db1e877bc6
Thomas Shaw <shawtr@umich.edu>
2019-11-09 02:53:54 -0500

    expectedPairs and expectedCrossPairs -- first try
    These are more or less just a direct rewrite, hopefully clearer
    and less cluttered, of the old cross_f_mc.funxy. The interface
    is also somewhat cleaned up, so that this function can accept
    funxy or im arguments, and mixes thereof
    
    Also fixed test, and fleshed out docs a bit more

--- pcfinhom/R/expected_pairs.R
+++ pcfinhom/R/expected_pairs.R
@@ -2,11 +2,142 @@
 # These are numerical estimates of the quatities f and f_12 in the paper.
 # Perhaps a better name would be "expected number of pairs for poisson process(es)".
 # Hence, naming these functions expectedPairs and the like
-cross_f <- function(rho1, rho2, hx, hy=NULL, dx=.01, ...) UseMethod("cross_f")
 
-f_est <- function(rho, hx, hy=NULL, dx=.01) cross_f(rho, rho, hx, hy, dx)
+# expectedPairs is just a wrapper for expectedCrossPairs that takes the right args
+expectedPairs <- function(rho, hx, hy=NULL, method=c("mc", "lattice"), tol=.005,
+                            dx=diff(as.owin(rho1)$xrange)/200, maxeval=1e6,
+                            maxsamp=5e3) {
+    expectedCrossPairs(rho1=rho, rho2=NULL, hx, hy, method, tol, dx,
+                        maxsamp=maxsamp, maxeval=maxeval)
+}
 
-cross_f.im <- function(rho1, rho2, hx, hy=NULL, dx=.01, ...) {
+expectedCrossPairs <- function(rho1, rho2=NULL, hx, hy=NULL, method=c("mc", "lattice"),
+                            tol=.005, dx=diff(as.owin(rho1)$xrange)/200,
+                            maxeval=1e6, maxsamp=5e3) {
+
+    # Validate arguments
+    stopifnot(is.im(rho1) || inherits(rho1, "funxy"))
+    if (is.im(rho1)) rho1 <- funxy(function(x,y) interp.im(rho1, x, y), as.owin(rho1))
+    cross <- !is.null(rho2)
+    if (cross) {
+        stopifnot(is.im(rho2) || inherits(rho2, "funxy"))
+        if (is.im(rho2)) rho2 <- funxy(function(x,y) interp.im(rho2, x, y), as.owin(rho2))
+    } else {
+        rho2 <- rho1 # for convenience later
+    }
+    method <- match.arg(method, c("mc", "lattice"))
+
+    use.mc <- FALSE
+    use.lattice <- FALSE
+    if (method == "mc") {
+        use.mc <- TRUE
+        stopifnot(is.numeric(tol) && tol > 0)
+    } else if (method == "lattice") {
+        use.lattice <- TRUE
+        stopifnot(is.numeric(dx) && dx > 0 && dx < as.owin(rho1))
+    }
+
+    # Check windows...
+    W <- as.owin(rho1)
+    if (cross) {
+        W2 <- as.owin(rho2)
+        stopifnot(W$xrange == W2$xrange && W$yrange == W2$yrange)
+    }
+
+    # Get coordinates of h
+    xy <- xy.coords(hx, hy)
+    allhx <- xy$x
+    allhy <- xy$y
+    nh <- length(hx)
+
+    # allocate results
+    epr <- numeric(length(hx))
+    epr2 <- numeric(length(hx))
+    sampwts <- numeric(length(hx))
+
+    if (is.rectangle(W)) {
+        winwts <- (abs(diff(W$xrange)) - abs(allhx))*(abs(diff(W$yrange)) - abs(allhy))
+    } else {
+        winwts <- numeric(length(allhx))
+        for (i in 1:length(allhx)) {
+            W2 <- shift(W, - c(allhx[i], allhy[i]))
+
+            winwts[i] <- overlap.owin(W, W2)
+        }
+    }
+
+    # state of looping
+    valid_h <- which(winwts > 0) # if winwts<=0 then f(h) = 0
+    nloop <- 0
+    inds <- valid_h # hs that haven't converged
+    nh_active <- length(valid_h)
+    if (nh_active == 0) return(eps) # none of the hs are valid
+
+    # Main loop
+    while (nh_active > 0) {
+        weights <- sampwts[inds]
+        hx <- allhx[inds]
+        hy <- allhy[inds]
+
+        # how many samples to take this iteration?
+        n_samp <- max(1, min(floor(maxeval/nh_active), maxsamp))
+
+        # Monte carlo sample of points in W
+        U <- runifpoint(n_samp, W)
+
+        # Corresponding intensity(s)
+        rho1U <- rho1(U)
+        rho2U <- if (cross) rho2(U) else rho1U
+
+        # Locations of second point, for each h
+        Uplushx <- outer(U$x, hx, `+`)
+        Uplushy <- outer(U$y, hy, `+`)
+        Uminushx <- outer(U$x, hx, `-`)
+        Uminushy <- outer(U$y, hy, `-`)
+
+        # Are these in the window?
+        Uplus_inside <- inside.owin(Uplushx, Uplushy, W)
+        Uminus_inside <- inside.owin(Uminushx, Uminushy, W)
+
+        # Where yes, corresponding intensity(s)
+        rhoUplus <- matrix(0, nrow=n_samp, ncol=nh_active)
+        rhoUminus <- matrix(0, nrow=n_samp, ncol=nh_active)
+        rhoUplus[Uplus_inside] <- rho2(Uplushx[Uplus_inside], Uplushy[Uplus_inside])
+        rhoUminus[Uminus_inside] <- rho1(Uminushx[Uminus_inside], Uminushy[Uminus_inside])
+
+        # Number of new points for each h?
+        weights <- weights + colSums(Uplus_inside + Uminus_inside)
+        sampwts[inds] <- weights
+
+        # Update estimates
+        epr[inds] <- (epr[inds]
+                        + (rho1U %*% rhoUplus)
+                        + (rho2U %*% rhoUminus))
+
+        epr2[inds] <- (epr2[inds]
+                        + ((rho1U^2) %*% (rhoUplus^2))
+                        + ((rho2U^2) %*% (rhoUminus^2)))
+
+        # estimate the standard error of the estimates
+        sd_est <- ( sqrt( (weights*epr2[inds] - epr[inds]^2) / (weights - 1))
+                            / epr[inds])
+
+        passed <- sd_est < tol
+        inds <- inds[!passed]
+        nh_active <- length(inds)
+        nloop <- nloop + 1
+
+
+        print(c(nh_active, epr[inds[1]], epr2[inds[1]], min(weights), max(weights), min(sd_est), max(sd_est)),
+                digits=2)
+    }
+    # Final output. sampwts is the number of applicable monte carlo samples
+    # winwts is the area of W \cap W_{-h}
+    epr[valid_h] <- epr[valid_h] * winwts[valid_h] / sampwts[valid_h]
+    epr
+}
+
+cross_f_lattice <- function(rho1, rho2, hx, hy=NULL, dx=.01, ...) {
 
     # Get coordinates of h
     xy <- xy.coords(hx, hy)
@@ -61,197 +192,4 @@ cross_f.im <- function(rho1, rho2, hx, hy=NULL, dx=.01, ...) {
     }
 
     return(val)
-}
-
-cross_f.funxy <- function(rho1, rho2, hx, hy=NULL, dx, ...) {
-
-    # Get coordinates of h
-    xy <- xy.coords(hx, hy)
-    hx <- xy$x
-    hy <- xy$y
-
-    val <- numeric(length(hx))
-
-    # Check windows
-    stopifnot(inherits(rho2, "funxy"))
-    W <- as.owin(rho1)
-    W2 <- as.owin(rho2)
-    stopifnot(W$xrange == W2$xrange && W$yrange == W2$yrange)
-
-    # get number of points on an appropriate lattice
-    xrange <- W$xrange
-    yrange <- W$yrange
-    nx <- ceiling(diff(xrange)/dx)
-    ny <- ceiling(diff(yrange)/dx)
-    dx <- diff(xrange)/nx
-    dy <- diff(yrange)/ny
-
-    #compute points of lattice
-    xs <- seq(xrange[1] + dx/2, xrange[2], by=dx);
-    ys <- seq(yrange[1] + dy/2, yrange[2], by=dy);
-    
-    latticex <- as.vector(outer(xs, ys, function(x,y) x))
-    latticey <- as.vector(outer(xs, ys, function(x,y) y))
-
-    # remove points not in window
-    inside <- inside.owin(latticex, latticey, W)
-    latticex <- latticex[inside]
-    latticey <- latticey[inside]
-
-    # and associated rho
-    latticez <- rho1(latticex, latticey)
-
-    # figure out how many hs per call to rho2:
-    ncall <- min(ceiling(length(latticez)* length(hx) / 1e6), length(hx))
-    npercall <- ceiling(length(hx)/ncall)
-
-    for (i in 1:ncall) {
-        # Which inds to get \rho for this time:
-        hinds <- (1 + (i-1)*npercall):min(i*npercall, length(hx))
-
-        # and rho(x + h)
-        allxs <- outer(latticex, hx[hinds], `+`)
-        allys <- outer(latticey, hy[hinds], `+`)
-        sz <- dim(allxs)
-        allxs <- as.vector(allxs)
-        allys <- as.vector(allys)
-
-        inside <- inside.owin(allxs, allys, W)
-        allrhos <- numeric(length(allxs))
-        allrhos[!inside] <- NA
-        
-        # densityfun warns when any points are outside of W
-        tmprhos <- suppressWarnings( rho2(as.vector(allxs), as.vector(allys)) )
-
-        if (length(tmprhos) == sum(inside)) {
-            # densityfun only returns values if they're inside W
-            allrhos[inside] <- tmprhos
-        } else if (length(tmprhos) == length(allxs)) {
-            # manually constructed funxy may return garbage outside of W
-            allrhos[inside] <- tmprhos[inside]
-        } else {
-            stop('rho2 returned unexpected number of values')
-        }
-
-        dim(allrhos) <- sz
-
-        # foreach xs
-        for (j in 1:length(hinds)) {
-            increment <- dx*dy*sum(latticez*allrhos[,j],na.rm=TRUE)
-            val[hinds[j]] <- increment
-        }
-    }
-    return(val)
-}
-
-# Another version of f, using monte-carlo
-cross_f_mc.funxy <- function(rho1, rho2, hx, hy=NULL, tol=.005) {
-
-    # get the coordinates
-    xy <- xy.coords(hx, hy)
-    hx <- xy$x
-    hy <- xy$y
-    o.out <- order(hx^2 + hy^2)
-    hx <- hx[o.out]
-    hy <- hy[o.out]
-
-    val <- numeric(length(hx))
-    val2 <- numeric(length(hx))
-    allwts <- numeric(length(hx))
-
-    # Check windows
-    W <- as.owin(rho1)
-    W2 <- as.owin(rho2)
-    stopifnot(W$xrange == W2$xrange && W$yrange == W2$yrange)
-
-    # Monte-carlo scheme
-    nh <- length(hx)
-    nloop <- 0
-    discrep <- Inf
-    firsth <- 1
-    inds <- firsth:nh
-
-    while (TRUE) {
-        weights <- allwts[inds]
-        nh_active <- length(inds)
-
-        # so you never have too many fevals per iteration
-        n_per_iter <- min(5000, ceiling(1e6/nh_active))
-        
-        U <- runifpoint(n_per_iter, W)
-
-        rho1U <- rho1(U)
-        rho2U <- rho2(U)
-
-        thesehx <- hx[inds]
-        thesehy <- hy[inds]
-
-        # add and subtract h from the random samples
-        Uphx <- outer(U$x, thesehx, `+`)
-        Uphy <- outer(U$y, thesehy, `+`)
-        Umhx <- outer(U$x, thesehx, `-`)
-        Umhy <- outer(U$y, thesehy, `-`)
-
-        insidep <- inside.owin(Uphx, Uphy, W)
-        insidem <- inside.owin(Umhx, Umhy, W)
-        rho2Uph <- matrix(0, nrow=n_per_iter, ncol=nh_active)
-        rho1Umh <- matrix(0, nrow=n_per_iter, ncol=nh_active)
-
-        #sequential version (one line)
-        rho2Uph[insidep] <- rho2(Uphx[insidep], Uphy[insidep])
-        rho1Umh[insidem] <- rho1(Umhx[insidem], Umhy[insidem])
-        
-        nperhp <- colSums(insidep)
-        nperhm <- colSums(insidem)
-        nperh <- nperhp + nperhm
-
-        nperup <- rowSums(insidep)
-        nperum <- rowSums(insidem)
-
-        oldval <- val[inds]/weights
-        val[inds] <-  (val[inds]
-                        + (rho1U %*% rho2Uph)
-                        + (rho2U %*% rho1Umh))
-
-        val2[inds] <- (val2[inds]
-                        + (rho1U)^2 %*% (rho2Uph)^2
-                        + (rho2U)^2 %*% (rho1Umh)^2)
-
-        weights <- weights + nperh
-
-        allwts[inds] <- weights
-
-        var_est <- sqrt(val2[inds] - val[inds]^2 /(weights-1)) / val[inds]
-
-        passed <- var_est < tol
-        npassed <- match(FALSE, passed) - 1
-        nh_active <- sum(!passed)
-
-        nloop <- nloop + 1
-        print(c(n_per_iter, nh_active,
-                min(nperum), max(nperum),
-                min(var_est), max(var_est)), digits=2)
-
-        if (is.na(npassed)) { #all elements passed
-            break
-        } else {
-            firsth <- firsth + npassed
-            inds <- inds[!passed] #firsth:nh
-        }
-    }
-
-    winwts <- numeric(length(hx))
-    if (is.rectangle(W)) {
-        winwts <- (abs(diff(W$xrange)) - abs(hx))*(abs(diff(W$yrange)) - abs(hy))
-    } else {
-        for (i in 1:length(hx)) {
-            W2 <- shift(W, - c(hx[i], hy[i]))
-
-            winwts[i] <- overlap.owin(W, W2)
-        }
-    }
-
-    val <- val * winwts / allwts
-    val[o.out] <- val #Sort back to original order
-    val
 }
